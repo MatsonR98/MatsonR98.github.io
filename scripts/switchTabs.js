8f5be2565a3cebdfd14bb2d5a77ecc7a213fc7d5
MatsonR98 <matson.ryan98@gmail.com>
2021-05-11 19:56:35 -0400

    Added hidden sections

--- scripts/switchTabs.js
+++ scripts/switchTabs.js
@@ -1,12 +1,33 @@
 function currentTab(element){
     var currentTab = document.getElementById(element);
 
+    var about_me = document.getElementById("aboutMe");
+    var projects = document.getElementById("projects");
+    var otherThings = document.getElementById("otherThings");
+    var contact = document.getElementById("contact");
+
     if(element === "aboutMeButton"){
+        about_me.style.display = "flex";
+        projects.style.display = "none";
+        otherThings.style.display = "none";
+        contact.style.display = "none";
     }
     else if(element === "projectsButton"){
+        about_me.style.display = "none";
+        projects.style.display = "flex";
+        otherThings.style.display = "none";
+        contact.style.display = "none";
     }
     else if(element === "otherThingsButton"){
+        about_me.style.display = "none";
+        projects.style.display = "none";
+        otherThings.style.display = "flex";
+        contact.style.display = "none";
     }
     else if(element === "contactButton"){
+        about_me.style.display = "none";
+        projects.style.display = "none";
+        otherThings.style.display = "none";
+        contact.style.display = "flex";
     }
 }
