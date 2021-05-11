ea6d3693e8aa97d2d35028b92d8bda9228564a69
MatsonR98 <matson.ryan98@gmail.com>
2021-05-11 19:38:42 -0400

    Added scripts and personal info

---
+++ scripts/switchTabs.js
@@ -0,0 +1,12 @@
+function currentTab(element){
+    var currentTab = document.getElementById(element);
+
+    if(element === "aboutMeButton"){
+    }
+    else if(element === "projectsButton"){
+    }
+    else if(element === "otherThingsButton"){
+    }
+    else if(element === "contactButton"){
+    }
+}
